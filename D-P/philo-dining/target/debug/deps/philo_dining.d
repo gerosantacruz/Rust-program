626ee48af3145551b724c388751b6b5a5b4e2464
gerosantacruz <geronimo.santacruz2017@gmail.com>
2020-02-06 17:54:37 -0300

    Philosoohy dining complete

---
+++ D-P/philo-dining/target/debug/deps/philo_dining.d
@@ -0,0 +1,5 @@
+C:\Users\geros\desktop\git\rust-test\D-P\philo-dining\target\debug\deps\philo_dining.exe: src\main.rs
+
+C:\Users\geros\desktop\git\rust-test\D-P\philo-dining\target\debug\deps\philo_dining.d: src\main.rs
+
+src\main.rs:
---
+++ D-P/philo-dining/target/debug/philo-dining.d
@@ -0,0 +1,1 @@
+C:\Users\geros\desktop\git\rust-test\D-P\philo-dining\target\debug\philo-dining.exe: C:\Users\geros\desktop\git\rust-test\D-P\philo-dining\src\main.rs
